8c3cc5654844b41881625fd6b2c0e3a62e41deeb
Sangeethm04 <43912785+Sangeethm04@users.noreply.github.com>
2021-04-17 17:27:05 -0400

    testing user if statement

--- index2.js
+++ index2.js
@@ -1,3 +1,4 @@
+var email_id = user.email;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -8,11 +9,12 @@ firebase.auth().onAuthStateChanged(function(user) {
     var user = firebase.auth().currentUser;
 
     if(user != null){
-
-      var email_id = user.email;
       document.getElementById("user_para").innerHTML = "Welcome User: " + email_id;
+      console.log("Welcome: " + email_id);
 
+      
     }
+     
 
   } else {
     // No user is signed in.
